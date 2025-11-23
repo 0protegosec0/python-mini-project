f5ce42d95195237608df8b66ec7155b0629529c9
hesams2002 <hesamsafari57@gmail.com>
2025-11-23 22:36:15 +0330

    chore: add time function and random array for test

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -1,6 +1,7 @@
 '''
 quick sort algorithm for learning how does it work
 '''
+import time
 import random
 
 def quick_sort(array):
@@ -15,5 +16,16 @@ def quick_sort(array):
     
     return quick_sort(lower) + equal + quick_sort(higher)
 
-test_array = [3,25,10,7,64,13,58,19,21,34,1,2,8,9]
-print(quick_sort(test_array))
+def time_quick_sort(array):
+    start = time.time()
+    result = quick_sort(array)
+    end = time.time()
+    
+    to_take = end - start
+    print(f"Execution time = {to_take: .6f} seconds")
+    return result
+
+
+test_array = [random.randint(1,1000) for _ in range(1000)]
+sorted_array = time_quick_sort(test_array)
+print("sorted array =", sorted_array)
